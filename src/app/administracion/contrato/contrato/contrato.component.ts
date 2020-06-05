0aaf66cc1a36e86fddc4bd2dbf913629a44d9145
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-06-05 12:42:05 -0400

    se agrega tabla a contrato

--- src/app/administracion/contrato/contrato/contrato.component.ts
+++ src/app/administracion/contrato/contrato/contrato.component.ts
@@ -3,6 +3,24 @@ import { Component, OnInit } from "@angular/core";
 declare const require: any;
 declare const $: any;
 
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
+declare interface TableWithCheckboxes {
+  id?: number;
+  ischecked?: boolean;
+  product_name: string;
+  type: string;
+  quantity: number;
+  price: any;
+  amount: string;
+}
+
+export interface TableData2 {
+  headerRow: string[];
+  dataRows: TableWithCheckboxes[];
+}
 @Component({
   selector: "app-contrato",
   templateUrl: "./contrato.component.html",
@@ -11,7 +29,7 @@ declare const $: any;
 export class ContratoComponent implements OnInit {
   simpleSlider = 40;
   doubleSlider = [20, 60];
-
+  public tableData1: TableData;
   regularItems = ["Pizza", "Pasta", "Parmesan"];
   touch: boolean;
 
@@ -29,7 +47,18 @@ export class ContratoComponent implements OnInit {
     { value: "moscow-6", viewValue: "Moscow" },
   ];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tableData1 = {
+      headerRow: ["#", "Name", "Job Position", "Since", "Salary", "Actions"],
+      dataRows: [
+        ["1", "Andrew Mike", "Develop", "2013", "99,225", ""],
+        ["2", "John Doe", "Design", "2012", "89,241", "btn-round"],
+        ["3", "Alex Mike", "Design", "2010", "92,144", "btn-link"],
+        ["4", "Mike Monday", "Marketing", "2013", "49,990", "btn-round"],
+        ["5", "Paul Dickens", "Communication", "2015", "69,201", ""],
+      ],
+    };
+  }
   myFunc(val: any) {
     // code here
   }
