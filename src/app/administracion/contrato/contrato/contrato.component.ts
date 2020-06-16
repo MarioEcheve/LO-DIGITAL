0e2fa73acaf65fadbd64a89a7085b14252b27167
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-06-16 14:21:34 -0400

    cambio de vistas en contrato

--- src/app/administracion/contrato/contrato/contrato.component.ts
+++ src/app/administracion/contrato/contrato/contrato.component.ts
@@ -1,26 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 declare const require: any;
 declare const $: any;
 
-declare interface TableData {
-  headerRow: string[];
-  dataRows: string[][];
-}
-declare interface TableWithCheckboxes {
-  id?: number;
-  ischecked?: boolean;
-  product_name: string;
-  type: string;
-  quantity: number;
-  price: any;
-  amount: string;
-}
-
-export interface TableData2 {
-  headerRow: string[];
-  dataRows: TableWithCheckboxes[];
-}
 @Component({
   selector: "app-contrato",
   templateUrl: "./contrato.component.html",
@@ -29,11 +12,10 @@ export interface TableData2 {
 export class ContratoComponent implements OnInit {
   simpleSlider = 40;
   doubleSlider = [20, 60];
-  public tableData1: TableData;
-  public tableData2: TableData;
+
   regularItems = ["Pizza", "Pasta", "Parmesan"];
   touch: boolean;
-
+  type: FormGroup;
   selectedValue: string;
   currentCity: string[];
 
@@ -47,99 +29,255 @@ export class ContratoComponent implements OnInit {
     { value: "barcelona-5", viewValue: "Barcelona" },
     { value: "moscow-6", viewValue: "Moscow" },
   ];
-
+  constructor(private formBuilder: FormBuilder) {}
   ngOnInit() {
-    this.tableData1 = {
-      headerRow: [
-        "Código",
-        "Nombre",
-        "Tipo Libro",
-        "Tipo Firma",
-        "Estado",
-        "Acción",
-      ],
-      dataRows: [
-        [
-          "LM01",
-          "Libro Principal",
-          "Maestro",
-          "Digital Avanzada",
-          "En creación",
-          "btn-link",
-        ],
-        [
-          "LC01",
-          "Libro de Comuniaciones",
-          "Auxiliar",
-          "Digital Simple",
-          "Abierto",
-          "btn-link",
-        ],
-        [
-          "LA01",
-          "Libro Prevención de Riesgos",
-          "Auxiliar",
-          "Por Sistema",
-          "Abierto",
-          "btn-link",
-        ],
-        ["LA02", "Libro PAC", "Auxiliar", "Por Sistema", "Abierto", "btn-link"],
-        [
-          "LA03",
-          "Libro Administrativo",
-          "Auxiliar",
-          "Por Sistema",
-          "Cerrado",
-          "btn-link",
-        ],
-      ],
-    };
+    const elemMainPanel = <HTMLElement>document.querySelector(".main-panel");
 
-    this.tableData2 = {
-      headerRow: ["RUT", "Nombre", "Cargo", "Perfil", "Estado", "Acción"],
-      dataRows: [
+    this.type = this.formBuilder.group({
+      // To add a validator, we must first convert the string value into an array. The first item in the array is the default value if any, then the next item in the array is the validator. Here we are adding a required validator meaning that the firstName attribute must have a value in it.
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [
+        null,
         [
-          "15.547.454-6",
-          "Nombre ApellidoP ApellidoM",
-          "Inspector Fiscal",
-          "Administrador",
-          "Activo",
-          "btn-link",
-        ],
-        [
-          "14.774.524-3",
-          "Nombre ApellidoP ApellidoM",
-          "Asistente ITO",
-          "Asistente",
-          "Abierto",
-          "btn-link",
-        ],
-        [
-          "12.774.524-3",
-          "Nombre ApellidoP ApellidoM",
-          "Inpector Fiscal (S)",
-          "Administrador (s)",
-          "Activo",
-          "btn-link",
-        ],
-        [
-          "16.774.524-3",
-          "Nombre ApellidoP ApellidoM",
-          "Jefe Area Construcción",
-          "Superior",
-          "Activo",
-          "btn-link",
-        ],
-        [
-          "18.774.524-3",
-          "Nombre ApellidoP ApellidoM",
-          "Secretaria Administrativa",
-          "Visita",
-          "Inactivo",
-          "btn-link",
+          Validators.required,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"),
         ],
       ],
-    };
+    });
+    // Code for the Validator
+    const $validator = $(".card-wizard form").validate({
+      rules: {
+        firstname: {
+          required: true,
+          minlength: 3,
+        },
+        lastname: {
+          required: true,
+          minlength: 3,
+        },
+        email: {
+          required: true,
+          minlength: 3,
+        },
+      },
+
+      highlight: function (element) {
+        $(element)
+          .closest(".form-group")
+          .removeClass("has-success")
+          .addClass("has-danger");
+      },
+      success: function (element) {
+        $(element)
+          .closest(".form-group")
+          .removeClass("has-danger")
+          .addClass("has-success");
+      },
+      errorPlacement: function (error, element) {
+        $(element).append(error);
+      },
+    });
+
+    // Wizard Initialization
+    $(".card-wizard").bootstrapWizard({
+      tabClass: "nav nav-pills",
+      nextSelector: ".btn-next",
+      previousSelector: ".btn-previous",
+
+      onNext: function (tab, navigation, index) {
+        var $valid = $(".card-wizard form").valid();
+        if (!$valid) {
+          $validator.focusInvalid();
+          return false;
+        }
+      },
+
+      onInit: function (tab: any, navigation: any, index: any) {
+        // check number of tabs and fill the entire row
+        let $total = navigation.find("li").length;
+        let $wizard = navigation.closest(".card-wizard");
+
+        let $first_li = navigation.find("li:first-child a").html();
+        let $moving_div = $('<div class="moving-tab">' + $first_li + "</div>");
+        $(".card-wizard .wizard-navigation").append($moving_div);
+
+        $total = $wizard.find(".nav li").length;
+        let $li_width = 100 / $total;
+
+        let total_steps = $wizard.find(".nav li").length;
+        let move_distance = $wizard.width() / total_steps;
+        let index_temp = index;
+        let vertical_level = 0;
+
+        let mobile_device = $(document).width() < 600 && $total > 3;
+
+        if (mobile_device) {
+          move_distance = $wizard.width() / 2;
+          index_temp = index % 2;
+          $li_width = 50;
+        }
+
+        $wizard.find(".nav li").css("width", $li_width + "%");
+
+        let step_width = move_distance;
+        move_distance = move_distance * index_temp;
+
+        let $current = index + 1;
+
+        if ($current == 1 || (mobile_device == true && index % 2 == 0)) {
+          move_distance -= 8;
+        } else if (
+          $current == total_steps ||
+          (mobile_device == true && index % 2 == 1)
+        ) {
+          move_distance += 8;
+        }
+
+        if (mobile_device) {
+          let x: any = index / 2;
+          vertical_level = parseInt(x);
+          vertical_level = vertical_level * 38;
+        }
+
+        $wizard.find(".moving-tab").css("width", step_width);
+        $(".moving-tab").css({
+          transform:
+            "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
+          transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)",
+        });
+        $(".moving-tab").css("transition", "transform 0s");
+      },
+
+      onTabClick: function (tab: any, navigation: any, index: any) {
+        const $valid = $(".card-wizard form").valid();
+
+        if (!$valid) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+      onTabShow: function (tab: any, navigation: any, index: any) {
+        let $total = navigation.find("li").length;
+        let $current = index + 1;
+        elemMainPanel.scrollTop = 0;
+        const $wizard = navigation.closest(".card-wizard");
+
+        // If it's the last tab then hide the last button and show the finish instead
+        if ($current >= $total) {
+          $($wizard).find(".btn-next").hide();
+          $($wizard).find(".btn-finish").show();
+        } else {
+          $($wizard).find(".btn-next").show();
+          $($wizard).find(".btn-finish").hide();
+        }
+
+        const button_text = navigation
+          .find("li:nth-child(" + $current + ") a")
+          .html();
+
+        setTimeout(function () {
+          $(".moving-tab").text(button_text);
+        }, 150);
+
+        const checkbox = $(".footer-checkbox");
+
+        if (index !== 0) {
+          $(checkbox).css({
+            opacity: "0",
+            visibility: "hidden",
+            position: "absolute",
+          });
+        } else {
+          $(checkbox).css({
+            opacity: "1",
+            visibility: "visible",
+          });
+        }
+        $total = $wizard.find(".nav li").length;
+        let $li_width = 100 / $total;
+
+        let total_steps = $wizard.find(".nav li").length;
+        let move_distance = $wizard.width() / total_steps;
+        let index_temp = index;
+        let vertical_level = 0;
+
+        let mobile_device = $(document).width() < 600 && $total > 3;
+
+        if (mobile_device) {
+          move_distance = $wizard.width() / 2;
+          index_temp = index % 2;
+          $li_width = 50;
+        }
+
+        $wizard.find(".nav li").css("width", $li_width + "%");
+
+        let step_width = move_distance;
+        move_distance = move_distance * index_temp;
+
+        $current = index + 1;
+
+        if ($current == 1 || (mobile_device == true && index % 2 == 0)) {
+          move_distance -= 8;
+        } else if (
+          $current == total_steps ||
+          (mobile_device == true && index % 2 == 1)
+        ) {
+          move_distance += 8;
+        }
+
+        if (mobile_device) {
+          let x: any = index / 2;
+          vertical_level = parseInt(x);
+          vertical_level = vertical_level * 38;
+        }
+
+        $wizard.find(".moving-tab").css("width", step_width);
+        $(".moving-tab").css({
+          transform:
+            "translate3d(" + move_distance + "px, " + vertical_level + "px, 0)",
+          transition: "all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)",
+        });
+      },
+    });
+
+    // Prepare the preview for profile picture
+    $("#wizard-picture").change(function () {
+      const input = $(this);
+
+      if (input[0].files && input[0].files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function (e: any) {
+          $("#wizardPicturePreview")
+            .attr("src", e.target.result)
+            .fadeIn("slow");
+        };
+        reader.readAsDataURL(input[0].files[0]);
+      }
+    });
+
+    $('[data-toggle="wizard-radio"]').click(function () {
+      const wizard = $(this).closest(".card-wizard");
+      wizard.find('[data-toggle="wizard-radio"]').removeClass("active");
+      $(this).addClass("active");
+      $(wizard).find('[type="radio"]').removeAttr("checked");
+      $(this).find('[type="radio"]').attr("checked", "true");
+    });
+
+    $('[data-toggle="wizard-checkbox"]').click(function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this).find('[type="checkbox"]').removeAttr("checked");
+      } else {
+        $(this).addClass("active");
+        $(this).find('[type="checkbox"]').attr("checked", "true");
+      }
+    });
+
+    $(".set-full-height").css("height", "auto");
   }
 
   myFunc(val: any) {
