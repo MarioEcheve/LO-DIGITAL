847ba1ecb2c3d2f2db2b1f05f82454465179ce58
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-07-09 17:46:11 -0400

    pdf

--- src/app/administracion/folio/folio-detalle/folio-detalle.component.ts
+++ src/app/administracion/folio/folio-detalle/folio-detalle.component.ts
@@ -41,6 +41,7 @@ export class FolioDetalleComponent implements OnInit {
   base64textString: string = "";
   usuario;
   muestraImagenes = false;
+  folioSiguiente;
   // IMPLEMENTACION CONFIG ANGULAR-EDITOR
   editorConfig: AngularEditorConfig = {
     editable: true,
@@ -144,12 +145,12 @@ export class FolioDetalleComponent implements OnInit {
     };
     let idFolio = this.route.snapshot.paramMap.get("id");
     this.buscarFolio(idFolio);
-
     //this.buscarFolioPorLibro(idLibro);
   }
   buscarFolio(id) {
     this.folioService.find(id).subscribe((respuesta) => {
       this.Folio = respuesta.body;
+      this.buscaCorrelativoFolio();
       let usuarioActual = JSON.parse(localStorage.getItem("user"));
       this.obtenerPerfilLibroUsuario(this.Folio.libro.id, usuarioActual.id);
       this.folioForm.controls["asunto"].setValue(respuesta.body.asunto);
@@ -436,29 +437,476 @@ export class FolioDetalleComponent implements OnInit {
     this.tipoFolioSeleccionado = tipo;
   }
   previsualizar() {
-    var imagenLogo1 = getBase64Image(document.getElementById("imagenLogo1"));
-    var imagenLogo2 = getBase64Image(document.getElementById("imagenLogo2"));
-    console.log(imagenLogo1);
+    let imagenLogo1 = getBase64Image(document.getElementById("imagenLogo1"));
+    let imagenLogo2 = getBase64Image(document.getElementById("imagenLogo2"));
+    let anotacion = stripHtml(this.folioForm.controls["anotacion"].value);
+
+    console.log(anotacion);
+    //console.log(imagenLogo1);
     var docDefinition = {
       content: [
         {
-          alignment: "justify",
-          columns: [
-            {
-              image: "data:image/png;base64," + imagenLogo1,
-              width: 40,
-              absolutePosition: { x: 50, y: 50 },
+          absolutePosition: { x: 50, y: 50 },
+          style: "tableExample",
+          table: {
+            widths: ["60%", "0%", "40%"],
+            body: [
+              [
+                {
+                  image: "bee",
+                  width: 60,
+                  height: 50,
+                },
+                "",
+                "        ",
+              ],
+              [
+                {
+                  stack: [
+                    {
+                      text: "Contrato: " + this.libro.contrato.nombre,
+                      italics: true,
+                    },
+                    {
+                      text: "Codigo: " + this.libro.contrato.codigo,
+                      italics: true,
+                    },
+                    {
+                      text: [
+                        { text: "Mandante : ", italics: true },
+                        {
+                          text: this.libro.contrato.dependenciaMandante.entidad
+                            .nombre,
+                          italics: true,
+                        },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Contratista : ", italics: true },
+                        {
+                          text: this.dependenciaContratista.entidad.nombre,
+                          italics: true,
+                        },
+                      ],
+                    },
+                  ],
+                },
+                /* a nested table will appear here as soon as I fix a bug */
+                [""],
+                {
+                  stack: [
+                    {
+                      text: "Libro: " + this.libro.nombre,
+                      italics: true,
+                    },
+                    { text: "Codigo: " + this.libro.codigo, italics: true },
+                    {
+                      text: "Clase Libro: " + this.libro.tipoLibro.descripcion,
+                      italics: true,
+                    },
+                    {
+                      text: [
+                        { text: "Tipo Firma: ", italics: true },
+                        { text: this.libro.tipoFirma.nombre, italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha Apertura: ", italics: true },
+                        {
+                          text: this.datePipe.transform(
+                            this.libro.fechaApertura,
+                            "dd-MM-yyyy hh:mm"
+                          ),
+                          italics: true,
+                        },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha cierre: ", italics: true },
+                        {
+                          text: this.datePipe.transform(
+                            this.libro.fechaCierre,
+                            "dd-MM-yyyy hh:mm"
+                          ),
+                          italics: true,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return i === 0 || i === node.table.body.length ? 0 : 0.2;
             },
-
-            {
-              image: "data:image/png;base64," + imagenLogo2,
-              width: 100,
-              absolutePosition: { x: 90, y: 57 },
+            vLineWidth: function (i, node) {
+              return 0;
             },
-          ],
-          columnGap: 5,
+            hLineColor: function (i, node) {
+              return i === 0 || i === node.table.body.length ? "red" : "grey";
+            },
+            vLineColor: function (i, node) {
+              return i === 0 || i === node.table.widths.length ? "red" : "grey";
+            },
+          },
+        },
+        {
+          absolutePosition: { x: 50, y: 200 },
+          style: "tableExample",
+          table: {
+            widths: ["60%", "0%", "40%"],
+            body: [
+              ["", "", "        "],
+              [
+                {
+                  stack: [
+                    {
+                      text:
+                        "Folio" +
+                        "#" +
+                        this.folioSiguiente +
+                        ": " +
+                        this.datePipe.transform(Date.now(), "dd-MM-yyyy hh:mm"),
+                      italics: true,
+                    },
+                    {
+                      text:
+                        "Emisor: " + this.folioForm.controls["emisor"].value,
+                      italics: true,
+                    },
+                    {
+                      text: [
+                        { text: "Tipo de Folio : ", italics: true },
+                        {
+                          text: this.folioForm.controls["tipoFolio"].value,
+                          italics: true,
+                        },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Respuesta de: ", italics: true },
+                        { text: "n/a  ", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Referencia de: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha requerida: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Asunto: ", italics: true },
+                        {
+                          text: this.folioForm.controls["asunto"].value,
+                          italics: true,
+                        },
+                      ],
+                    },
+                  ],
+                },
+                /* a nested table will appear here as soon as I fix a bug */
+                [""],
+                {
+                  /*
+                  stack: [
+                    {
+                      text: "Libro: " + "Libro Axiliar",
+                      italics: true,
+                    },
+                    { text: "Clase Libro: " + "Axiliar", italics: true },
+                    {
+                      text: [
+                        { text: "Tipo Firma: ", italics: true },
+                        { text: "Digital Simple  ", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha Apertura: ", italics: true },
+                        { text: "08-07-2020 11:59", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha cierre: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                  ],
+                  */
+                },
+              ],
+            ],
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return i === 0 || i === node.table.body.length ? 0 : 0.2;
+            },
+            vLineWidth: function (i, node) {
+              return 0;
+            },
+            hLineColor: function (i, node) {
+              return i === 0 || i === node.table.body.length ? "red" : "grey";
+            },
+            vLineColor: function (i, node) {
+              return i === 0 || i === node.table.widths.length ? "red" : "grey";
+            },
+          },
+        },
+        {
+          absolutePosition: { x: 50, y: 320 },
+          style: "tableExample",
+          table: {
+            widths: ["60%", "0%", "40%"],
+            body: [
+              ["", "", "        "],
+              [
+                {
+                  stack: [
+                    {
+                      text: "2 " + "Archivos Adjuntos",
+                      italics: true,
+                    },
+                    { text: "Archivo 1  " + "Archivo 2", italics: true },
+                  ],
+                },
+                /* a nested table will appear here as soon as I fix a bug */
+                [""],
+                {
+                  /*
+                  stack: [
+                    {
+                      text: "Libro: " + "Libro Axiliar",
+                      italics: true,
+                    },
+                    { text: "Clase Libro: " + "Axiliar", italics: true },
+                    {
+                      text: [
+                        { text: "Tipo Firma: ", italics: true },
+                        { text: "Digital Simple  ", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha Apertura: ", italics: true },
+                        { text: "08-07-2020 11:59", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha cierre: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                  ],
+                  */
+                },
+              ],
+            ],
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return i === 0 || i === node.table.body.length ? 0 : 0.01;
+            },
+            vLineWidth: function (i, node) {
+              return 0;
+            },
+            hLineColor: function (i, node) {
+              return i === 0 || i === node.table.body.length ? "red" : "grey";
+            },
+            vLineColor: function (i, node) {
+              return i === 0 || i === node.table.widths.length ? "red" : "grey";
+            },
+          },
+        },
+        {
+          absolutePosition: { x: 50, y: 370 },
+          style: "tableExample",
+          table: {
+            widths: ["90%", "0%", "0%"],
+            body: [
+              ["", "", "        "],
+              [
+                {
+                  stack: [
+                    {
+                      text: "Fernando vilchez",
+                      italics: true,
+                    },
+                    { text: "Administrador de contrato", italics: true },
+                    {
+                      text: "Fecha Firma:" + " 09-07-2020 15:46",
+                      italics: true,
+                    },
+                    { text: "Firma Digital Avanzada", italics: true },
+                    {
+                      text:
+                        "Cód. Verificación: d5sd4537dasd45675asd456ad-7856asd-745",
+                      italics: true,
+                    },
+                  ],
+                },
+                /* a nested table will appear here as soon as I fix a bug */
+                [""],
+                {
+                  /*
+                  stack: [
+                    {
+                      text: "Libro: " + "Libro Axiliar",
+                      italics: true,
+                    },
+                    { text: "Clase Libro: " + "Axiliar", italics: true },
+                    {
+                      text: [
+                        { text: "Tipo Firma: ", italics: true },
+                        { text: "Digital Simple  ", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha Apertura: ", italics: true },
+                        { text: "08-07-2020 11:59", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha cierre: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                  ],
+                  */
+                },
+              ],
+            ],
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return i === 0 || i === node.table.body.length ? 0 : 0.01;
+            },
+            vLineWidth: function (i, node) {
+              return 0;
+            },
+            hLineColor: function (i, node) {
+              return i === 0 || i === node.table.body.length ? "red" : "grey";
+            },
+            vLineColor: function (i, node) {
+              return i === 0 || i === node.table.widths.length ? "red" : "grey";
+            },
+          },
+        },
+        {
+          absolutePosition: { x: 50, y: 460 },
+          style: "tableExample",
+          table: {
+            widths: ["100%", "0%", "0%"],
+            body: [
+              ["", "", "        "],
+              [
+                {
+                  stack: [
+                    {
+                      text: "Anotacion",
+                      italics: true,
+                    },
+                    {
+                      text: anotacion,
+                      italics: true,
+                    },
+                  ],
+                },
+                /* a nested table will appear here as soon as I fix a bug */
+                [""],
+                {
+                  /*
+                  stack: [
+                    {
+                      text: "Libro: " + "Libro Axiliar",
+                      italics: true,
+                    },
+                    { text: "Clase Libro: " + "Axiliar", italics: true },
+                    {
+                      text: [
+                        { text: "Tipo Firma: ", italics: true },
+                        { text: "Digital Simple  ", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha Apertura: ", italics: true },
+                        { text: "08-07-2020 11:59", italics: true },
+                      ],
+                    },
+                    {
+                      text: [
+                        { text: "Fecha cierre: ", italics: true },
+                        { text: "", italics: true },
+                      ],
+                    },
+                  ],
+                  */
+                },
+              ],
+            ],
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return i === 0 || i === node.table.body.length ? 0 : 0.01;
+            },
+            vLineWidth: function (i, node) {
+              return 0;
+            },
+            hLineColor: function (i, node) {
+              return i === 0 || i === node.table.body.length ? "red" : "grey";
+            },
+            vLineColor: function (i, node) {
+              return i === 0 || i === node.table.widths.length ? "red" : "grey";
+            },
+          },
+        },
+        {
+          text: "",
+          pageBreak: "after",
         },
       ],
+      images: {
+        bee: "data:image/png;base64," + imagenLogo1,
+        logo2: "data:image/png;base64," + imagenLogo2,
+      },
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5],
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15],
+          width: 500,
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: "black",
+        },
+      },
     };
     const pdfDocGenerator = pdfMake.createPdf(docDefinition);
     let url;
@@ -486,6 +934,13 @@ export class FolioDetalleComponent implements OnInit {
     this.base64textString = btoa(binaryString);
     console.log(btoa(binaryString));
   }
+  buscaCorrelativoFolio() {
+    this.folioService
+      .correlativoFolio(this.Folio.libro.id)
+      .subscribe((respuesta) => {
+        this.folioSiguiente = respuesta.body[0].numero_folio;
+      });
+  }
 }
 function getBase64Image(img) {
   var canvas = document.createElement("canvas");
@@ -495,4 +950,12 @@ function getBase64Image(img) {
   ctx.drawImage(img, 0, 0);
   var dataURL = canvas.toDataURL("image/png");
   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+function stripHtml(html) {
+  // Create a new div element
+  var temporalDivElement = document.createElement("div");
+  // Set the HTML content with the providen
+  temporalDivElement.innerHTML = html;
+  // Retrieve the text property of the element (cross-browser support)
+  return temporalDivElement.textContent || temporalDivElement.innerText || "";
 }
