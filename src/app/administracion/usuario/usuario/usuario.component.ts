0383af8556a99092007236ea57d494492f1d4f1f
Mario Echeverria lopez <marioandreseche@gmail.com>
2020-09-25 16:56:16 -0300

    elminiacion filtro de listado de contrato

--- src/app/administracion/usuario/usuario/usuario.component.ts
+++ src/app/administracion/usuario/usuario/usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UsuarioDependenciaService } from '../../services/usuario-dependencia.service';
+import { UsuarioDependencia } from '../../TO/usuario-dependencia.model';
 
 @Component({
   selector: 'app-usuario',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
-
-  constructor() { }
+  usuarioDependenciaActual : UsuarioDependencia;
+  formUsuario : FormGroup;
+  constructor( private usuarioDependenciaService : UsuarioDependenciaService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.cargaUsuarioDependenciaActual();
   }
-
+  async cargaUsuarioDependenciaActual(){
+    let usuario = JSON.parse(localStorage.getItem("user"));
+    await new Promise(()=>{
+      this.usuarioDependenciaService.findUserByUsuarioDependencia(usuario.id).subscribe(
+        usuarioDependencia=>{
+          this.usuarioDependenciaActual = usuarioDependencia.body[0];
+          this.inicializarForm(this.usuarioDependenciaActual)
+        }
+      )
+    });
+  }
+  inicializarForm(usuarioDependencia : UsuarioDependencia){
+    this.formUsuario = this.fb.group({
+      rut : ['hola'] 
+    });
+  }
 }
